bbba2494d0b430fe45a8d29608f5d5b5a48ce628
Wtorres27 <costellobenida@gmail.com>
2023-12-03 12:23:34 -0800

    part 8

--- controllers/places.js
+++ controllers/places.js
@@ -1,101 +1,120 @@
 const router = require("express").Router();
-const places = require("../models/places.js");
+const db = require("../models");
+
+
+//INDEX (view all places)
 
 router.get("/", (req, res) => {
-  res.render("places/index", { places });
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
 });
+
+function name () {
+
+
+}
+
+//CREATE (post new place to db)
+
+router.post("/", (req, res) => {
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+
+
+//NEW (view form to create new place)
 
 router.get("/new", (req, res) => {
-  res.render("places/new");
+  res.render("places/new")
+})
+
+
+
+//SHOW (view one place)
+
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
 
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
+
+//UPDATE (update place in db)
+
+router.put("/:id", (req, res) => {
+  console.log(req.params.id);
+  db.Place.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(
+      () => {
+        res.redirect(`/places/${req.params.id}`);
+      })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
 
 
-router.post('/', (req, res) => {
- 
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
+//EDIT (view form to edit place)
 
-router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
+router
+  .get("/:id/edit", (req, res) => {
+    db.Place.findById(req.params.id)
+      .then((place) => {
+        res.render("places/edit", { place });
+      })
+      .catch((err) => {
+        console.log("err", err);
+        res.render("error404");
+      });
+  });
 
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
-})
+
+//DELETE (delete place from db)
+router
+  .delete("/:id", (req, res) => {
+    db.Place.
+      findByIdAndDelete(req.params.id)
+      .then(() => {
+        res.redirect("/places");
+      })
+      .catch((err) => {
+        console.log("err", err);
+        res.render("error404");
+      });
+  });
 
 
 
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})
+//FOR AFTER PART 8
 
+// router.post('/:id/rant', (req, res) => {
+//   res.send('GET /places/:id/rant stub')
+// })
+
+// router.delete('/:id/rant/:rantId', (req, res) => {
+//     res.send('GET /places/:id/rant/:rantId stub')
+// })
 
 
 module.exports = router;
